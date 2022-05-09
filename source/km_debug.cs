aabf0234921cba25b8052fa4977b21d3ecd90b3b
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-05-09 01:03:32 -0500

    Update km_debug.cs

--- source/km_debug.cs
+++ source/km_debug.cs
@@ -20,7 +20,12 @@
 //  */
 //
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
+using KSP.IO;
+using KSP;
 
 namespace KlockheedMartianTools
 {
